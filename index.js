0d206bdbffcbb05935791214d9abe40151d3a116
Sam Dean <samdena09@gmail.com>
2020-05-09 00:54:13 +0100

    more stuff

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 const dbName = './data.json';
+resetDB();
 const db = require(dbName);
 
 var play_deck = '';
 var max_players = 8;
 var current_players;
+var hands;
+var dealer;
+var small_blind;
+var big_blind;
+var active_player;
+var pot;
+var active_game = false;
 
 app.get('/', function(req, res) {
   res.render('index.ejs');
@@ -18,32 +26,72 @@ app.get('/dealer23101996', function(req, res) {
   res.render('dealer.ejs');
 });
 
-function newGame() {
+function startGame() {
+  active_game = true;
+  newHand();
+}
+
+function newHand() {
+  hands++;
+  dealCards();
+  pot = 0;
+
+  let player;
+
+  dealer = 'player' + (hands+0)%current_players;
+  player = db.players[dealer].name
+  io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + player + ' is the dealer.');
+
+  small_blind = 'player' + (hands+1)%current_players;
+  player = db.players[small_blind].name
+  io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + player + ' is the small blind.');
+
+  big_blind = 'player' + (hands+2)%current_players;
+  player = db.players[big_blind].name
+  io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + player + ' is the big blind.');
+
+  active_player = 'player' + (hands+3)%current_players;
+  player = db.players[active_player].name
+  io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: it\'s '+ player + '\'s turn.');
+
+  updateActivePlayer();
+}
+
+function resetDB() {
   fs.copyFile('./default_data.json', dbName, function copyErr(err) {
     if (err) return console.log(err);
     io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + 'Game Ready!');
   });
+}
 
+function newGame() {
+  active_game = false;
+  resetDB();
   play_deck = newDeck();
   //console.log(play_deck);
   current_players = 0;
+  hands = 0;
 }
 
 io.sockets.on('connection', function(socket) {
   socket.on('username', function(username) {
     socket.username = username;
-    console.log('id' + socket.id);
+    console.log('id ' + socket.id);
     if (!(username == 'Dealer')) {
       let player_type_message = '';
-      if (current_players < max_players) {
+      if (current_players < max_players && !active_game) {
         let playerNumberText = 'player' + current_players;
         db.players[playerNumberText].id = socket.id;
+        db.players[playerNumberText].chips = 10000;
+        db.players[playerNumberText].name = username;
         writeToDB();
+        updateChips(socket.id, 10000);
         current_players++;
         player_type_message = 'as player ' + current_players;
       } else {
         player_type_message = 'as a spectator ';
       }
+      updateActivePlayer();
       io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat ' + player_type_message + '</i>');
       console.log(socket.username + ' joined')
     }
@@ -63,43 +111,26 @@ io.sockets.on('connection', function(socket) {
     newGame();
   })
 
-  socket.on('deal_cards', function() {
+  socket.on('start_game', function() {
+    startGame();
+  })
 
-    for (let i = 0; i < current_players; i++) {
-      let playerNum = 'player' + i;
-      db.players[playerNum].card0 = getCard();
-      db.players[playerNum].card1 = getCard();
-    }
-    /*
-    db.players.player0.card1 = getCard();
-    db.players.player0.card2 = getCard();
-    db.players.player1.card1 = getCard();
-    db.players.player1.card2 = getCard();
-    db.players.player2.card1 = getCard();
-    db.players.player2.card2 = getCard();
-    db.players.player3.card1 = getCard();
-    db.players.player3.card2 = getCard();
-    db.players.player4.card1 = getCard();
-    db.players.player4.card2 = getCard();
-    db.players.player5.card1 = getCard();
-    db.players.player5.card2 = getCard();
-    db.players.player6.card1 = getCard();
-    db.players.player6.card2 = getCard();
-    db.players.player7.card1 = getCard();
-    db.players.player7.card2 = getCard();
-    */
-    writeToDB();
-
-    //console.log(io.sockets);
-    for (let i = 0; i < current_players; i++) {
-      let playerNumberText = 'player' + i;
-      let socketId = db.players[playerNumberText].id;
-      io.to(socketId).emit('hey', 'I just met you');
-    }
-
-  });
+  socket.on('new_hand', function() {
+    newHand();
+  })
 
 });
+
+function updateChips(id, chips) {
+  io.to(id).emit('chips', chips);
+}
+
+function updateActivePlayer() {
+  if (active_player) {
+    io.emit('active_player', db.players[active_player].name);
+    io.to(db.players[active_player].id).emit('active_player', 'YOU!');
+  }
+}
 
 function writeToDB() {
   fs.writeFile(dbName,JSON.stringify(db, null, 2), function writeJSON(err) {
@@ -107,7 +138,6 @@ function writeToDB() {
     //console.log(JSON.stringify(db));
     console.log('writing to ' + dbName)
     //console.log(play_deck);
-    io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + 'Cards Dealt!');
   });
 }
 
@@ -143,4 +173,27 @@ function shuffle(array) {
 function newDeck() {
   let new_deck = deck;
   return shuffle(new_deck);
+}
+
+
+function dealCards() {
+
+  for (let i = 0; i < current_players; i++) {
+    let playerNum = 'player' + i;
+    db.players[playerNum].card0 = getCard();
+    db.players[playerNum].card1 = getCard();
+  }
+  writeToDB();
+
+  //console.log(io.sockets);
+  for (let i = 0; i < current_players; i++) {
+    let playerNumberText = 'player' + i;
+    let socketId = db.players[playerNumberText].id;
+    let cards = [db.players[playerNumberText].card0, db.players[playerNumberText].card1];
+    io.to(socketId).emit('cards', cards);
+  }
+
+
+  io.emit('chat_message', '<strong>' + 'Dealer' + '</strong>: ' + 'Cards Dealt!');
+
 }
